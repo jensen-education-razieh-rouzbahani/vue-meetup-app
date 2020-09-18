ab8faa0b0aa5f933ba5de3843323662947e531d7
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-09-18 21:03:07 +0200

    data added to store & modified app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app>
-    
+    <v-toolbar>
+      <v-toolbar-title>Meetup</v-toolbar-title>
+      <v-toolbar-items>
+        <v-btn>View Meetups</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main></main>
   </v-app>
 </template>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    loadedMeetups: [
+      { imageUrl: 'https://www.costacruises.se/content/dam/costa/inventory-assets/ports/STO/sto-stoccolma-port-1.jpg.image.750.563.low.jpg',
+      id: '1', 
+      title: 'Meetup in Stockholm',
+      date: new Date(),
+      location: 'Stockholm',
+      description:'Beautiful city'
+    },
+      { imageUrl: 'https://www.telegraph.co.uk/content/dam/Travel/2018/May/gothenburg-hall.jpg',
+      id: '2', 
+      title: 'Meetup in Gothenburg',
+      date: new Date(),
+      location: 'Gothenburg',
+      description:'Nice city'
+    }
+    ],
   },
   mutations: {
   },
