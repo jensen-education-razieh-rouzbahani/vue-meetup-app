16f9cce604c21c0d6772d80eb5c4f1079e957fb9
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-09-28 17:42:28 +0200

    added some css

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import '@mdi/font/css/materialdesignicons.css' 
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdiSvg', // default - only for display purposes
+  },
   theme: {
       options: {
         customProperties: true,
